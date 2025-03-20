49f559a2ef498185effe649aad9119b6e5aa5d2a
ClementRollin <clemrollin01@gmail.com>
2025-03-20 15:16:34 +0100

    Change project to Gantt personalize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,7 @@ export const metadata: Metadata = {
     description: 'Ajoutez vos tâches et visualisez un diagramme de Gantt complet.',
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="fr">
         <body>{children}</body>
